5bee6004d4f50df9869c0450a82ea3a65b1d5f1f
사용자 방문 <Guest@dit05ui-imac.local>
2019-03-27 00:42:00 -0700

    123

--- imageView Animation Timer 01/ViewController.swift
+++ imageView Animation Timer 01/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    var count = 1
+    // var isAnimation = false
+    var myTimer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageview.image = UIImage(named: "Image1")
+        countLabel.text = String(count)
     }
-
-
+    @IBAction func playbtn(_ sender: Any) {
+        // Timer 호출
+        myTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(doAnimation), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pausebtn(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @IBAction func stopbtn(_ sender: Any) {
+        myTimer.invalidate()
+        count = 1
+        imageview.image = UIImage(named: "Image1")
+        countLabel.text = String(count)
+    }
+    
+    @objc func doAnimation(){
+        if count == 5 {
+            count = 1
+        } else {
+            count += 1
+        }
+        
+        imageview.image = UIImage(named: "Image\(count)")
+        countLabel.text = String(count)
+        
+    }
 }
 
